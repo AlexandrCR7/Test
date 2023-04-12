702266507e20916fc70990492c086d7cde0d1cab
Alexandr <k9108@mail.ru>
2023-04-12 16:15:06 +0300

    add

---
+++ HW10 v1/Program.cs
@@ -0,0 +1,6 @@
+﻿System.Console.WriteLine("Введите трехзначное число ");
+int number = int.Parse(System.Console.ReadLine());
+
+int number1 = number / 10;
+int number2 = number1 % 10;
+System.Console.WriteLine(number2);
