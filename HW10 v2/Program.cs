b14c6f6d4e13f4976bd6e89bf8b398b85ee6f674
Alexandr <k9108@mail.ru>
2023-04-12 16:20:31 +0300

    Add

---
+++ HW10 v2/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.WriteLine("Введите трехзначное число ");
+int number;
+while(!int.TryParse(System.Console.ReadLine(), out number)){
+    System.Console.WriteLine("Это не число, Алле!!!");
+}
+
+int number1 = number / 10;
+int number2 = number1 % 10;
+System.Console.WriteLine(number2);
